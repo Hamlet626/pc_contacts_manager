e4a70aed11b395bdb32a8829c39934b134c90878
wenjianli <wenjianli@umail.ucsb.edu>
2024-02-21 18:05:37 -0800

    create group dialog

--- lib/screens/overview/overview.dart
+++ lib/screens/overview/overview.dart
@@ -16,7 +16,7 @@ class OverviewScreen extends HookConsumerWidget {
         _buildView('All Matching GCs',const GCsView(),tt),
         const SizedBox(width: 30,),
         _buildView('All Wechat Groups',const WcGroupsView(),tt,
-            action: ElevatedButton(
+            action: FilledButton(
                 onPressed: ()=>showDialog(context: context, builder: (context)=>const CreateGroupDialog()),
                 child: const Text('Create'))),
       ],)),);
