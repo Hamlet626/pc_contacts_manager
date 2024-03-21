f2381a926e2bef5bc45592133b9c6ad2e433abff
wenjianli <wenjianli@umail.ucsb.edu>
2024-03-20 19:10:55 -0700

    send n logo profiles

--- lib/screens/overview/gcs_dialog.dart
+++ lib/screens/overview/gcs_dialog.dart
@@ -23,44 +23,50 @@ class GcDialog extends HookConsumerWidget {
           width: 400,
           child:GCsView(selector: selector,dtbGroup: group,)),
       actions: [
-        TextButton(onPressed: selector.value.isEmpty||loading.value?null:()async{
-          loading.value=true;
-          //todo: send api /gc
-
-          final res=await Future.wait(selector.value.map((gcid)async{
-            try{
-              final distRes=await post(Uri.parse('https://us-central1-pc-application-portal.cloudfunctions.net/distributeGcProfile'),
-                  headers: {'cKey': 'hamlet','Content-Type': 'application/json'},
-                  body: json.encode({
-                    'gcId':gcid,
-                    'groups':[group['topic']],
-                    'updateFirebase':true
-                  }));
-              final jsonRes=json.decode(distRes.body);
-              return {'success':jsonRes['success_groups']!=null,'message':jsonRes['message']};
-            }catch(e,st){
-              return {'success':false,'message':'unknown error:$e'};}
-          }));
-
-          if(res.every((e) => e['success'])) {
-            Navigator.pop(context);
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                backgroundColor: Colors.greenAccent,
-                content: Text('successfully sent profiles!')));
-          } else {
-            loading.value=false;
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                backgroundColor: Colors.red,
-                content: Text('sending \n${
-                    (List.generate(res.length, (i){
-                      if(res[i]['success'])return null;
-                      final gc=allgcs!.singleWhere((e) => e['id']== selector.value[i]);
-                      return '${gc['First_Name']} ${gc['Last_Name']}: ${res[i]['message']}';
-                    })..removeWhere((v)=>v==null)).join(',\n')
-                }\n failed!')));
-          }
-        }, child: Text('${selector.value.isEmpty?'':'${selector.value.length} distributes'} Confirm'))
+        TextButton(onPressed: selector.value.isEmpty||loading.value?null:()=>sendProfile(context,loading,selector,allgcs!,false),
+            child: Text('${selector.value.isEmpty?'':'${selector.value.length} distributes'} Confirm (no Logo)')),
+        FilledButton(onPressed: selector.value.isEmpty||loading.value?null:()=>sendProfile(context,loading,selector,allgcs!,true),
+            child: Text('${selector.value.isEmpty?'':'${selector.value.length} distributes'} Confirm'))
       ],
     );
+  }
+
+  Future sendProfile(BuildContext context, ValueNotifier<bool>loading,
+      ValueNotifier<List<String>>selector, List<Map<String,dynamic>>allgcs, bool withLogo)async{
+    loading.value=true;
+
+    final res=await Future.wait(selector.value.map((gcid)async{
+      try{
+        final distRes=await post(Uri.parse('https://us-central1-pc-application-portal.cloudfunctions.net/distributeGcProfile'),
+            headers: {'cKey': 'hamlet','Content-Type': 'application/json'},
+            body: json.encode({
+              'gcId':gcid,
+              'groups':[group['topic']],
+              'updateFirebase':true,
+              'logo':withLogo
+            }));
+        final jsonRes=json.decode(distRes.body);
+        return {'success':jsonRes['success_groups']!=null,'message':jsonRes['message']};
+      }catch(e,st){
+        return {'success':false,'message':'unknown error:$e'};}
+    }));
+
+    if(res.every((e) => e['success'])) {
+      Navigator.pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          backgroundColor: Colors.greenAccent,
+          content: Text('successfully sent profiles!')));
+    } else {
+      loading.value=false;
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          backgroundColor: Colors.red,
+          content: Text('sending \n${
+              (List.generate(res.length, (i){
+                if(res[i]['success'])return null;
+                final gc=allgcs.singleWhere((e) => e['id']== selector.value[i]);
+                return '${gc['First_Name']} ${gc['Last_Name']}: ${res[i]['message']}';
+              })..removeWhere((v)=>v==null)).join(',\n')
+          }\n failed!')));
+    }
   }
 }
--- lib/screens/overview/groups_dialog.dart
+++ lib/screens/overview/groups_dialog.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:http/http.dart';
@@ -21,36 +22,42 @@ class WcGroupDialog extends HookConsumerWidget {
           width: 400,
           child:WcGroupsView(selector: selector,dtbGC: gc,)),
       actions: [
-        TextButton(onPressed: selector.value.isEmpty||loading.value?null:()async{
-          loading.value=true;
-          //todo: send api /gc
-          late Map<String,dynamic> data;
-          try{
-            final distRes=await post(Uri.parse('https://us-central1-pc-application-portal.cloudfunctions.net/distributeGcProfile'),
-                headers: {'cKey': 'hamlet','Content-Type': 'application/json'},
-                body: json.encode({
-                  'gcId':gc['id'],
-                  'groups':selector.value,
-                  'updateFirebase':true
-                }));
-            data=json.decode(distRes.body);
-          }catch(e,st){
-            data={'message':'Send profile error!\n$e'};
-          }
-
-          final success=data['success_groups']!=null&&data['success_groups'].length==selector.value.length;
-          if(success){
-            Navigator.pop(context);
-          }else{
-            loading.value=false;
-          }
-
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              duration: Duration(seconds: 30),
-              backgroundColor: success?Colors.greenAccent:Colors.red,
-              content: Text(data['message'],)));
-        }, child: Text('${selector.value.isEmpty?'':'${selector.value.length} distributes'} Confirm'))
+        TextButton(onPressed: selector.value.isEmpty||loading.value?null:()=>sendProfile(context,loading,selector,false),
+            child: Text('${selector.value.isEmpty?'':'${selector.value.length} distributes'} Confirm (no Logo)')),
+        FilledButton(onPressed: selector.value.isEmpty||loading.value?null:()=>sendProfile(context,loading,selector,true),
+            child: Text('${selector.value.isEmpty?'':'${selector.value.length} distributes'} Confirm'))
       ],
     );
+  }
+
+  sendProfile(BuildContext context,ValueNotifier<bool>loading,ValueNotifier<List<String>>selector,bool withLogo)async{
+    loading.value=true;
+
+    late Map<String,dynamic> data;
+    try{
+      final distRes=await post(Uri.parse('https://us-central1-pc-application-portal.cloudfunctions.net/distributeGcProfile'),
+          headers: {'cKey': 'hamlet','Content-Type': 'application/json'},
+          body: json.encode({
+            'gcId':gc['id'],
+            'groups':selector.value,
+            'updateFirebase':true,
+            'logo':withLogo
+          }));
+      data=json.decode(distRes.body);
+    }catch(e,st){
+      data={'message':'Send profile error!\n$e'};
+    }
+
+    final success=data['success_groups']!=null&&data['success_groups'].length==selector.value.length;
+    if(success){
+      Navigator.pop(context);
+    }else{
+      loading.value=false;
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        duration: Duration(seconds: 30),
+        backgroundColor: success?Colors.greenAccent:Colors.red,
+        content: Text(data['message'],)));
   }
 }
